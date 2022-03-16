271e9b9b385abc26076eadb84e830a9f00b837d6
ravishdubey <49018353+ravishdubey@users.noreply.github.com>
2022-03-16 15:36:03 +0530

    Create violin_plot.py

---
+++ violin_plot.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+df=pd.read_csv('grimmvsopc.csv')
+pd.options.display.float_format = "{:.2f}".format
